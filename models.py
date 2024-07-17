4dcd85591cb3785896dc483eb3924ac1fb768169
user.name <akamrannicole@gmail.com>
2024-07-17 20:40:00 +0300

    done

---
+++ models.py
@@ -0,0 +1,17 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    location = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+
+db.create_all()
